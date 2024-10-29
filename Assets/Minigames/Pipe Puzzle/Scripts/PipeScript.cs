36b225c0a0c902ed04486e1e4d366f067f7af6a6
dylanusinggithub <159280769+dylanusinggithub@users.noreply.github.com>
2024-10-29 10:36:05 +0000

    Reverse rotation successfully implemented

--- Assets/Minigames/Pipe Puzzle/Scripts/PipeScript.cs
+++ Assets/Minigames/Pipe Puzzle/Scripts/PipeScript.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PipeScript : MonoBehaviour
 {
-    float[] rotations = { 0, 90, 180, 270 };
+    //Rotations to be randomized
+    float[] rotations = { 0, 90, 180, 270};
 
+    //The correct upright rotation
     public float[] correctRotation;
 
+    //A bool to check if the rotation is correct and upright
     [SerializeField]
     bool isPlaced = false;
 
+    //The number of possible correct rotations
     int PossibleRotation = 1;
 
+    //A link the game manager
     GameManager gameManager;
 
+    //Visual representation of complete tile
     public GameObject FilledTile;
 
+    //Locate the game manager via the game manager object
     private void Awake()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -24,11 +31,15 @@ public class PipeScript : MonoBehaviour
 
     private void Start()
     {
+        //Set the length of correct rotations
         PossibleRotation = correctRotation.Length;
+        //Picks a random number from 0 to the number of angles that are viable
         int rand = Random.Range(0, rotations.Length);
 
+        //Randomize rotation based on z axis
         transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
 
+        //
         if (PossibleRotation > 1)
         {
             if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1])
@@ -86,6 +97,43 @@ public class PipeScript : MonoBehaviour
             {
                 isPlaced = false;
                 gameManager.wrongMove();
+            }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.Rotate(new Vector3(0, 0, -90));
+            transform.eulerAngles = new Vector3(0, 0, Mathf.Round(transform.eulerAngles.z));
+
+
+            if (PossibleRotation > 1)
+            {
+                if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] && isPlaced == false)
+                {
+                    isPlaced = true;
+                    gameManager.correctMove();
+                }
+                else if (isPlaced == true)
+                {
+                    isPlaced = false;
+                    gameManager.wrongMove();
+                }
+            }
+            else
+            {
+                if (transform.eulerAngles.z == correctRotation[0] && isPlaced == false)
+                {
+                    isPlaced = true;
+                    gameManager.correctMove();
+                }
+                else if (isPlaced == true)
+                {
+                    isPlaced = false;
+                    gameManager.wrongMove();
+                }
             }
         }
     }
