54958ce204556eaf297704ee35a7cf460adc5a0f
dylanusinggithub <dylan.yuen@gmail.com>
2024-11-05 01:46:30 +0000

    Timer/Score mechanic (incomplete)

---
+++ Assets/Minigames/Pipe Puzzle/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject youlose;
+    public GameObject score;
+    //Text scoretext;
+    public GameObject youwin;
+    public Image timer_image;
+    float time_remaining;
+    public float maxtime = 20f;
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        time_remaining = maxtime;
+        //scoretext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.gamewon == true)
+        {
+            youwin.SetActive(true);
+            score.SetActive(true);
+            //I'll add a counter instead assuming we're making it warioware style so just ++ points here
+            //scoretext.text=time_remaining.ToString("Your score is " + time_remaining);
+        }
+        else if (time_remaining > 0)
+        {
+            time_remaining -= Time.deltaTime;
+            timer_image.fillAmount = time_remaining / maxtime;
+        }
+        else
+        {
+            youlose.SetActive(true);
+        }
+    }
+}
