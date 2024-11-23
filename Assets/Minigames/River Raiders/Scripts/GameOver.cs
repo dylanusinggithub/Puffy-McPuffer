c1a93a7c080a891bda0c6f052397db1f774939a5
Jack Logan <130975290+30197124@users.noreply.github.com>
2024-11-23 21:09:11 +0000

    Game Over Panel Completion
    Game Over panel is now complete.  "Retry" button reloads the Canal Cruiser minigame, and "Quit" button takes player back to Level Select Map.

---
+++ Assets/Minigames/River Raiders/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverPanel; // Game Over panel
+
+    // Clicking on the "Retry" button
+    public void OnRetryButton() // If the player clicks the "Retry" button
+    {
+        SceneManager.LoadScene("River Raiders"); // Minigame is reloaded
+        Time.timeScale = 1f; // Time scale is reset to 1 (gameplay starts again)
+    }
+
+    // Clicking on the "Quit" button
+    public void OnQuitButton() // if the player clicks the "Quit" button
+    {
+        SceneManager.LoadScene("Level Select Map"); // Level Select screen is loaded
+        Time.timeScale = 1f; // Time scale is reset to 1
+    }
+}
