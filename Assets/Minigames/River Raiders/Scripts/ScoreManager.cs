e3e4094bb2fced531180485d5a95210d5ac40da6
TwinEwan <ewan239clark@gmail.com>
2024-11-24 17:24:43 +0000

    Reworked Canal Crusier's Movement
    Reworked Canal Crusier's Movement to not have instant turn arounds and be more "weighty" (lower acceleration)

--- Assets/Minigames/River Raiders/Scripts/ScoreManager.cs
+++ Assets/Minigames/River Raiders/Scripts/ScoreManager.cs
@@ -9,6 +9,11 @@ public class ScoreManager : MonoBehaviour
     private int score = 0; // Player's score is set at 0
     public GameObject cargo;
 
+    [SerializeField, Range(0f, 20f)]
+    int scoreWin;
+
+    public GameObject youWin;
+
     public int GetScore()
     {
         return score; // Provide access to the score
@@ -20,6 +25,12 @@ public class ScoreManager : MonoBehaviour
         score += points; // Add 1 point to game score
         score = Mathf.Max(score, 0); // Ensure score doesn't go below 0
         UpdateScoreText(); // Update Score Text function is called
+
+        if (scoreWin == score)
+        {
+            Time.timeScale = 0;
+            youWin.SetActive(true);
+        }
         cargo.SetActive(true);
     }
 
