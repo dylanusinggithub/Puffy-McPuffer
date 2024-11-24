a2a6ac4477d0a8d4ca9b055a68b5eb09a5b5cddb
Jack Logan <130975290+30197124@users.noreply.github.com>
2024-11-24 00:59:45 +0000

    You Win Panel
    Only managed to add in text and "Level Select" Button so far.  Still need to create a "YouWin" script.

---
+++ Assets/Minigames/River Raiders/Scripts/YouWin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouWin : MonoBehaviour
+{
+    public GameObject youWinPanel;
+
+    public void OnContinueButton()
+    {
+
+    }
+}
