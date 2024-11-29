8e773afd6b5d789ccfe329ba184a4662adb8ff4d
TwinEwan <ewan239clark@gmail.com>
2024-11-29 21:30:11 +0000

    Finished Object Script
    I forgor

--- Assets/Minigames/Canal Cruiser/Scripts/Object Script.cs
+++ Assets/Minigames/Canal Cruiser/Scripts/Object Script.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    bool randomiseScale;
+
+    [SerializeField, Range(-0.5f, 0.5f)]
+    float startSpeed = 0;
+
+    private void OnValidate()
     {
-        
+        if (randomiseScale)
+        {
+            transform.localScale = new Vector3(Random.Range(0.5f, 1.5f), transform.localScale.y, transform.localScale.z);
+            randomiseScale = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.Translate(new Vector2(0, startSpeed));   // don't ask why it's reversed, idk why
     }
 }
