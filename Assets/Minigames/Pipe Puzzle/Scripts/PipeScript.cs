d6709aae11a4d57a0d749e1dbd48a60a1ccda15b
dylanusinggithub <159280769+dylanusinggithub@users.noreply.github.com>
2024-12-03 10:19:08 +0000

    GIANT SHOEBILL

--- Assets/Minigames/Pipe Puzzle/Scripts/PipeScript.cs
+++ Assets/Minigames/Pipe Puzzle/Scripts/PipeScript.cs
@@ -31,8 +31,7 @@ public class PipeScript : MonoBehaviour
     public GameObject broke2;
     public GameObject broke0;
 
-    public ParticleSystem clickparticle;
-    private ParticleSystem clickinstance;
+    
 
     //Locate the game manager via the game manager object
     private void Awake()
@@ -106,7 +105,7 @@ public class PipeScript : MonoBehaviour
 
         clickcounter++;
         FindObjectOfType<AudioManager>().Play("Peeped");
-        spawnps();
+        
         if (PossibleRotation > 1)
         {
             if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] && isPlaced == false)
@@ -148,7 +147,7 @@ public class PipeScript : MonoBehaviour
             transform.eulerAngles = new Vector3(0, 0, Mathf.Round(transform.eulerAngles.z));
 
             clickcounter++;
-            spawnps();
+            
             if (PossibleRotation > 1)
             {
                 if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] && isPlaced == false)
@@ -182,8 +181,5 @@ public class PipeScript : MonoBehaviour
         }
     }
 
-    private void spawnps()
-    {
-            clickinstance = Instantiate(clickparticle, transform.position, Quaternion.identity);
-    }
+    
 }
