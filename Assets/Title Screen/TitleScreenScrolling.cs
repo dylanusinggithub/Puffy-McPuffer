9ba0a025ea897ecd2d2e6b9efca1002726dcfbde
Gretelandcat <yukkuri142@gmail.com>
2025-02-09 00:26:52 +0000

    Title Screen Save

--- Assets/Title Screen/TitleScreenScrolling.cs
+++ Assets/Title Screen/TitleScreenScrolling.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScreenScrolling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private RawImage scrollingImage;
+    [SerializeField] private float _x,_y;
+  
+    
     void Update()
     {
-        
+        scrollingImage.uvRect = new Rect(scrollingImage.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, scrollingImage.uvRect.size);
     }
 }
