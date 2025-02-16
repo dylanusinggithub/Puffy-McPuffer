71e822c4cad9b33aae9ba1262a0dc9f454037b67
KokwithoutaK <124071481+KokwithoutaK@users.noreply.github.com>
2025-02-16 23:01:38 +0000

    added new font to buttons and added transitions between menu screens

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,6 +8,8 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     int HowToIndex = 0;
+
+    [SerializeField] Animator FadeTransition;
 
     public void PlayGame()
     {
@@ -16,6 +20,8 @@ public class MainMenu : MonoBehaviour
 
     public void BTN_Main()
     {
+        HowToIndex = 0;
+        transform.GetChild(1).GetChild(5).gameObject.SetActive(false);
         foreach (Transform Menu in this.transform) Menu.gameObject.SetActive(false);
         transform.GetChild(0).gameObject.SetActive(true);
     }
@@ -23,10 +29,13 @@ public class MainMenu : MonoBehaviour
 
     public void BTN_HowTo()
     {
-        foreach (Transform Menu in this.transform) Menu.gameObject.SetActive(false);
-        transform.GetChild(1).gameObject.SetActive(true);
 
-        transform.GetChild(1).GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().text = "Next Page";
+        foreach (Transform Menu in this.transform) Menu.gameObject.SetActive(false);
+        StartCoroutine(FadeTime());
+
+        transform.GetChild(1).gameObject.SetActive(true);
+        
+        transform.GetChild(1).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>().text = "Next Page";
 
         transform.GetChild(1).GetChild(2).gameObject.SetActive(true);
         transform.GetChild(1).GetChild(3).gameObject.SetActive(true);
@@ -35,21 +44,31 @@ public class MainMenu : MonoBehaviour
 
     public void BTN_HowToNext()
     {
+        StartCoroutine(FadeTime());
         HowToIndex++;
-        if (HowToIndex == 1)
+        if (HowToIndex < 3 ) 
         {
-            transform.GetChild(1).GetChild(2).gameObject.SetActive(false);
-            transform.GetChild(1).GetChild(3).gameObject.SetActive(false);
-            transform.GetChild(1).GetChild(4).gameObject.SetActive(true);
+            if (HowToIndex == 2)
+            {
+
+                transform.GetChild(1).GetChild(2).gameObject.SetActive(false);
+            }
+            transform.GetChild(1).GetChild(HowToIndex + 2).gameObject.SetActive(false);
+            transform.GetChild(1).GetChild(HowToIndex + 3).gameObject.SetActive(true);
 
             transform.GetChild(1).GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().text = "Exit";
         }
-        else BTN_Main();
+        else
+        {
+            StartCoroutine(FadeTime());
+            BTN_Main();
+        }
     }
 
     public void BTN_Options()
     {
         foreach (Transform Menu in this.transform) Menu.gameObject.SetActive(false);
+        StartCoroutine(FadeTime());
         transform.GetChild(2).gameObject.SetActive(true);
     }
 
@@ -58,4 +77,15 @@ public class MainMenu : MonoBehaviour
         foreach (Slider Slider in transform.GetComponentsInChildren<Slider>()) 
             Slider.value = Slider.GetComponent<SliderController>().valueDefault;
     }
+
+    IEnumerator FadeTime()
+    {
+        FadeTransition.gameObject.SetActive(true);
+        //FadeTransition.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+        FadeTransition.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        FadeTransition.SetTrigger("Start");
+    }
+
+
 }
