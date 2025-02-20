8ce8b0d59c32347b5c70f7792cc8a95d7a34a3cd
Twin-Ewan <ewan239clark@gmail.com>
2025-02-20 18:34:20 +0000

    Merged/Redid Menu UI Scripts, Added Continue Button and Updated Lose Screen &

--- Assets/Minigames/Pipe Puzzle/Scripts/MenuControl.cs
+++ Assets/Minigames/Pipe Puzzle/Scripts/MenuControl.cs
@@ -1,35 +1,51 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MenuControl : MonoBehaviour
 {
-    public Button restart;
-    public Button menu;
     [SerializeField] Animator Fadetransition;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        restart.onClick.AddListener(restartgame);
-        menu.onClick.AddListener(openmenu);
-    }
+    [SerializeField] GameObject OptionsPanel, PausePanel;
+    [SerializeField] GameObject PauseBTN;
 
     // Update is called once per frame
-    void restartgame()
+    public void BTN_Restart()
     {
         StartCoroutine(FadeRetry());
         SceneManager.LoadScene("Pipelike");
         Time.timeScale = 1f;
     }
 
-    void openmenu()
+    public void BTN_ReturnToLevelSelect()
     {
         StartCoroutine(Fade());
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Level Select Map");
         Time.timeScale = 1f;
+    }
+
+    public void BTN_AdvanceToNextLevel()
+    {
+        LevelDesigner.AdvanceToNextLevel = true;
+        BTN_ReturnToLevelSelect();
+    }
+
+    public void BTN_PauseMenu()
+    {
+        PausePanel.SetActive(!PausePanel.activeInHierarchy); // Disables if enabled, and enables if disabled
+        
+        if(PausePanel.activeInHierarchy) Time.timeScale = 0f;
+        else Time.timeScale = 1f;
+    }
+
+    public void BTN_OptionsMenu()
+    {
+        bool isEnabled = OptionsPanel.activeInHierarchy;
+
+        // Disables all children only if it is currently closed, renables em when opened
+        foreach (Transform child in PausePanel.GetComponentInChildren<Transform>()) child.gameObject.SetActive(isEnabled);
+
+        OptionsPanel.SetActive(!isEnabled);
+        PauseBTN.SetActive(isEnabled);
     }
 
     IEnumerator Fade()
