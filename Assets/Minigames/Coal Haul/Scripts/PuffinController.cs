224187b7e9dd6d32f233a9233ffb7514459b750e
Jack Logan <130975290+30197124@users.noreply.github.com>
2025-03-07 11:59:50 +0000

    Game Over Panel & Preventing Dragging
    I managed to fix the issue with player dragging when Game Over panel is enabled in Coal Haul.
    
    At first, the player was still being dragged when Game Over panel was enabled after colliding with wall boundaries.
    
    After 5 or 6 failed solutions, I changed the body type in Rigidbody2D from Kinematic to Dynamic, and finally got the desired result.

--- Assets/Minigames/Coal Haul/Scripts/PuffinController.cs
+++ Assets/Minigames/Coal Haul/Scripts/PuffinController.cs
@@ -2,33 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class PuffinController : MonoBehaviour
 {
     private Vector3 offset;
     private Camera cam;
+    private bool isGameOver = false; //isGameOver boolean set to false
+
+    [SerializeField] private GameObject gameOverPanel; //Game Over panel object
 
     void Start()
     {
         cam = Camera.main;
+        gameOverPanel.SetActive(false);
     }
 
+
+    //On Mouse Down Function
     void OnMouseDown()
     {
+        if (isGameOver) return; // Stop input when Game Over
         offset = transform.position - Camera.main.ScreenToWorldPoint
             (new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
     }
 
 
+    //On Mouse Drag Function
     void OnMouseDrag()
     {
+        if (isGameOver) return; // Stop dragging when Game Over
         Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)) + offset;
         transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
     }
 
-    void OnCollisionEnter2D()
-    {
 
+    //If Puffin Collides With Any Walls
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            GameOver();  //Game Over FUnciton is called
+        }
+    }
+
+    //Game Over Function
+    void GameOver()
+    {
+        isGameOver = true; // isGameOver boolean set to true (prevent dragging)
+        UnityEngine.Debug.Log("Game Over! Dragging disabled.");
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>(); //Get Rigidbody2D Component
+
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(true); //Enable Game Over Panel
+        }
+
+        Time.timeScale = 0f; // Time scale is set to 0 (Game Pauses)
     }
 }
