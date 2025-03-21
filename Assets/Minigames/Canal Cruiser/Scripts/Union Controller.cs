ab060d548819224e9a5126f6b00cdd705ac20e24
Twin-Ewan <ewan239clark@gmail.com>
2025-03-21 22:28:26 +0000

    Added Proper Chase Mechanic to Canal Cruiser

--- Assets/Minigames/Canal Cruiser/Scripts/Union Controller.cs
+++ Assets/Minigames/Canal Cruiser/Scripts/Union Controller.cs
@@ -11,9 +11,10 @@ public class UnionController : MonoBehaviour
     GameObject Wave;
 
     int oldScore = 0;
-    float waterPecentage = 0;
+    [SerializeField] float waterPecentage = 0;
 
     public float chaseDistance;
+    float chaseOffset;
 
     void Start()
     {
@@ -35,6 +36,12 @@ public class UnionController : MonoBehaviour
             this.enabled = false;
             return;
         }
+        else if(Wave.transform.position.x > chaseDistance * 0.75f)
+        {
+            // Adds a offset to the minimum distance the Wave can be from Puffy starting at the last 25% it'll 
+            // start to come closer and closer till the last 5% where it'll be right against you
+            chaseOffset = Mathf.Lerp(0, 6, (Wave.transform.position.x - (chaseDistance * 0.75f)) / (chaseDistance * 0.20f));
+        }
 
         float dist = (Puffy.transform.position.y - Wave.transform.localPosition.y) / 20;
         if (Mathf.Abs(dist) * 20 > 0.1f) Velocity += dist; // Stops moving wave if not far away enough to stop oscillating
@@ -50,6 +57,10 @@ public class UnionController : MonoBehaviour
 
             oldScore = SS.score;
         }
+        else
+        {
+            Wave.transform.localPosition = new Vector2(Mathf.Lerp(3, -3 + chaseOffset, waterPecentage / SS.scoreWin), Wave.transform.localPosition.y);
+        }
     }
 
 
@@ -60,12 +71,10 @@ public class UnionController : MonoBehaviour
 
         if (MoveBack)
         {
-            for (int i = waterSmoothness; i > 0; i++)
+            for (int i = waterSmoothness; i > 0; i--)
             {
                 yield return new WaitForSeconds(waterSeconds / waterSmoothness);
                 waterPecentage -= waterSeconds / waterSmoothness;
-
-                Wave.transform.localPosition = new Vector2(Mathf.Lerp(3, -3, waterPecentage / SS.scoreWin), Wave.transform.localPosition.y);
             }
         }
         else
@@ -74,9 +83,9 @@ public class UnionController : MonoBehaviour
             {
                 yield return new WaitForSeconds(waterSeconds / waterSmoothness);
                 waterPecentage += waterSeconds / waterSmoothness;
-
-                Wave.transform.localPosition = new Vector2(Mathf.Lerp(3, -3, waterPecentage / SS.scoreWin), Wave.transform.localPosition.y);
             }
         }
+
+        waterPecentage = Mathf.RoundToInt(waterPecentage);
     }
 }
